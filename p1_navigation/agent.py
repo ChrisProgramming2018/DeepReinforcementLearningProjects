1996a9336384baf7027be66dbdbed3b549c36cdf
Christian Leininger <christianleininger42@gmail.com>
2019-03-13 01:12:19 -0700

    pylint checkstyle

--- p1_navigation/agent.py
+++ p1_navigation/agent.py
@@ -30,22 +30,22 @@ class Agent(object):
         self.n = args.multi_step
         self.discount = args.discount
 
-        self.online_net = DQN(args, self.state_size, self.action_size).to(device=args.device)
+        self.qnetwork_local = DQN(args, self.state_size, self.action_size).to(device=args.device)
         if args.model and os.path.isfile(args.model):
             # Always load tensors onto CPU by default, will shift to GPU if necessary
-            self.online_net.load_state_dict(torch.load(args.model, map_location='cpu'))
-        self.online_net.train()
+            self.qnetwork_local.load_state_dict(torch.load(args.model, map_location='cpu'))
+        self.qnetwork_local.train()
 
         self.target_net = DQN(args, self.state_size, self.action_size).to(device=args.device)
         self.update_target_net()
         self.target_net.train()
         for param in self.target_net.parameters():
             param.requires_grad = False
-        self.optimizer = optim.Adam(self.online_net.parameters(), lr=args.lr, eps=args.adam_eps)
+        self.optimizer = optim.Adam(self.qnetwork_local.parameters(), lr=args.lr, eps=args.adam_eps)
 
     def reset_noise(self):
         """ resets noisy weights in all linear layers """
-        self.online_net.reset_noise()
+        self.qnetwork_local.reset_noise()
 
     def act(self, state):
         """
@@ -54,7 +54,7 @@ class Agent(object):
              param1 (int) : state
         """
         with torch.no_grad():
-            return (self.online_net(state.unsqueeze(0).to(self.device)) * self.support).sum(2).argmax(1).item()
+            return (self.qnetwork_local(state.unsqueeze(0).to(self.device)) * self.support).sum(2).argmax(1).item()
 
     def act_e_greedy(self, state, epsilon=0.001):
         """ acts with epsilon greedy policy
@@ -74,12 +74,12 @@ class Agent(object):
         # Sample transitions
         idxs, states, actions, returns, next_states, nonterminals, weights = mem.sample(self.batch_size)
         # Calculate current state probabilities (online network noise already sampled)
-        log_ps = self.online_net(states, log=True)  # Log probabilities log p(s_t, *; theta online)
+        log_ps = self.qnetwork_local(states, log=True)  # Log probabilities log p(s_t, *; theta online)
         log_ps_a = log_ps[range(self.batch_size), actions]  # log p(s_t, a_t; theat online)
 
         with torch.no_grad():
             # Calculate nth next state probabilities
-            pns = self.online_net(next_states)  # Probabilities p(s_t+n, *; theta online)
+            pns = self.qnetwork_local(next_states)  # Probabilities p(s_t+n, *; theta online)
             dns = self.support.expand_as(pns) * pns  # Distribution d_t+n = (z, p(s_t+n, *; theat online))
             argmax_indices_ns = dns.sum(2).argmax(1)  # Perform argmax action selection using online network: argmax_a[(z, p(s_t+n, a;  theat online))]
             self.target_net.reset_noise()  # Sample new target net noise
@@ -103,7 +103,7 @@ class Agent(object):
             m.view(-1).index_add_(0, (u + offset).view(-1), (pns_a * (b - l.float())).view(-1))  # m_u = m_u + p(s_t+n, a*)(b - l)
 
         loss = -torch.sum(m * log_ps_a, 1)  # Cross-entropy loss (minimises DKL(m||p(s_t, a_t)))
-        self.online_net.zero_grad()
+        self.qnetwork_local.zero_grad()
         (weights * loss).mean().backward()  # Backpropagate importance-weighted minibatch loss
         self.optimizer.step()
 
@@ -116,35 +116,35 @@ class Agent(object):
         Args:
            param1 (float): tau
         """
-        for target_param, local_param in zip(self.target_net.parameters(), self.online_net.parameters()):
+        for target_param, local_param in zip(self.target_net.parameters(), self.qnetwork_local.parameters()):
             target_param.data.copy_(tau*local_param.data + (1.0-tau)*target_param.data)
 
     def update_target_net(self):
         """ copy the model weights from the online to the target network """
-        self.target_net.load_state_dict(self.online_net.state_dict())
+        self.target_net.load_state_dict(self.qnetwork_local.state_dict())
 
     def save(self, path):
         """ save the model weights to a file
         Args:
            param1 (string): pathname
         """
-        torch.save(self.online_net.state_dict(), os.path.join(path, 'model.pth'))
+        torch.save(self.qnetwork_local.state_dict(), os.path.join(path, 'model.pth'))
 
     def evaluate_q(self, state):
         """ Evaluates Q-value based on single state
         """
         with torch.no_grad():
-            return (self.online_net(state.unsqueeze(0)) * self.support).sum(2).max(1)[0].item()
+            return (self.qnetwork_local(state.unsqueeze(0)) * self.support).sum(2).max(1)[0].item()
 
     def train(self):
         """
         activates the backprob. layers for the online network
         """
-        self.online_net.train()
+        self.qnetwork_local.train()
 
     def eval(self):
         """ invoke the eval from the online network
             deactivates the backprob
             layers like dropout will work in eval model instead
         """
-        self.online_net.eval()
+        self.qnetwork_local.eval()
