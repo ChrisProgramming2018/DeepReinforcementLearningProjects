80afc85ce578d29ad07139d4982b9dd12ff02d81
Christian Leininger <christianleininger42@gmail.com>
2019-03-26 11:36:17 -0700

    pylint checkstyle

--- p3_collab-compet/env.py
+++ p3_collab-compet/env.py
@@ -1,12 +1,13 @@
 """ environment """
 
-from unityagents import UnityEnvironment
 import numpy as np
+from unityagents import UnityEnvironment
 
 
 class Env(object):
-    """ """
+    """ tennis environment from unity """
     def __init__(self):
+        """ init memeber variables   """
         self.env = UnityEnvironment(file_name='Tennis_Linux/Tennis.x86_64', no_graphics=True)
         self.brain_name = self.env.brain_names[0]
         self.brain = self.env.brains[self.brain_name]
@@ -14,24 +15,28 @@ class Env(object):
         self.action_size = self.brain.vector_action_space_size
         self.state_size = self.env_info.vector_observations.shape[1]
         self.num_agents = len(self.env_info.agents)
-    
+
     def reset(self):
-        """
+        """ resets the environment set the rewards to zero
+        Returns: states and scores array
         """
         env_info = self.env.reset(train_mode=True)[self.brain_name]
-        states = env_info.vector_observations           
+        states = env_info.vector_observations
         scores = np.zeros(self.num_agents)
         return states, scores
 
     def step(self, actions):
-        """ 
-        
+        """ takes the actions to take a step in the env
+            to get the reward and next_states.
+            dones bool if true the episode is over
+
         Args:
-            param1:
+            param1: (numpy)
+        Return: next_states , rewards, dones
         """
         env_info = self.env.step(actions)[self.brain_name]
         rewards = env_info.rewards
         next_states = env_info.vector_observations
         dones = env_info.local_done
-        
+
         return next_states, rewards, dones
