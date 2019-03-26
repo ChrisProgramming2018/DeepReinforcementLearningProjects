ec99f64124a0e7e0d94d22ca14e51bf1569c1b1a
Christian Leininger <christianleininger42@gmail.com>
2019-03-26 12:13:21 -0700

    pylint helper

--- p3_collab-compet/helper.py
+++ p3_collab-compet/helper.py
@@ -1,29 +1,34 @@
-""" Implementation of ddpg  """
+""" Implementation helper function for ddpg  """
+from collections import deque
+import time
 import numpy as np
 import matplotlib.pyplot as plt
+import torch
 
 
 
-
-
-def ddpg(env, ma, n_episodes=3000, max_t=2000):
-    """ 
-    
-    
+def ddpg(env, multi_agents, n_episodes=3000, max_t=2000):
+    """  use the env to train the agents until it reach the
+          goal score or run out of episodes return array of score of
+          each episode
+    Args:
+        param1: (Env) enviromnent
+        param2: (multi_agents)
+        param3: (int) episodes
+        param3: (int) steps each episode
+    Return: scores of all episodes
     """
 
     overall_scorces = []
-    score_window = deque(max_len(100)) # saves last 100 rewards
-    t0 = time.time()
-
+    score_window = deque(maxlen=100) # saves last 100 rewards
+    t_0 = time.time()
     for i_episode in range(1, n_episodes + 1):
-
-        ma.reset()
+        multi_agents.reset()
         states, scores = env.reset()
         for _ in range(max_t):
-            actions = ma.act(states)
-            next_states, actions, rewards, dones = env.step(action) 
-            ma.step(states, actions, rewards, next_states, dones)
+            actions = multi_agents.act(states)
+            next_states, rewards, dones = env.step(actions)
+            multi_agents.step(states, actions, rewards, next_states, dones)
             scores = scores + rewards
             states = next_states
 
@@ -31,25 +36,26 @@ def ddpg(env, ma, n_episodes=3000, max_t=2000):
         score_window.append(avg_score)
         overall_scorces.append(avg_score)
 
-        print('\r Epoisode {}\t Score: {:.2f}, Average Score: {:.2f}  Time: {:.2f}'.format(i_episode, np.mean(scores), np.mean(score_window), time.time() - t0))
+        print('\r Epoisode {}\t Score: {:.2f}, Average Score: {:.2f}  Time: {:.2f} buffer {}'.format(i_episode, avg_score, np.mean(score_window), time.time() - t_0, len(multi_agents.memory)))
         if np.mean(score_window) >= 0.5:
-            print('\nEnvironment solved in {:d} episodes!\tAverage Score: {:.2f}'.format(i_episode-5, np.mean(scores_window)))
-            print("Save Agensts"
-            torch.save(ma.ddpg_agents[0].actor_online.state_dict(), 'checkpoint_actor.pth')
-            torch.save(ma.ddpg_agents[0].critic_online.state_dict(), 'checkpoint_critic.pth')
+            print('\nEnvironment solved in {:d} episodes!\tAverage Score: {:.2f}'.format(i_episode-5, np.mean(score_window)))
+            print("Save Agensts")
+            torch.save(multi_agents.ddpg_agents[0].actor_online.state_dict(), 'checkpoint_actor.pth')
+            torch.save(multi_agents.ddpg_agents[0].critic_online.state_dict(), 'checkpoint_critic.pth')
             break
     return overall_scorces
 
 
 
 def plot_score(scores):
-    """   
+    """ creates an png file from the score and saves same dir
     Args:
-        param1:(list) scores of the episode 
+        param1:(list) scores of the episode
     """
     fig = plt.figure()
-    ax = fig.add_subplot(111)
+    fig.add_subplot(111)
     plt.plot(np.arange(len(scores)), scores)
     plt.ylabel('Score')
     plt.xlabel('Episode #')
+    plt.savefig('scores.png')
     plt.show()
