247b7b1d7c5ae9c752f442e34dd5a177a247b638
Christian Leininger <christianleininger42@gmail.com>
2019-03-26 13:45:38 -0700

    pylint checkstyle

--- p3_collab-compet/main.py
+++ p3_collab-compet/main.py
@@ -3,16 +3,12 @@ import argparse
 from helper import ddpg, plot_score
 from memory import ReplayBuffer
 from env import Env
-from multiAgent import MultiAgent
-
-
-
+from multi_agent import MultiAgent
 
 
 
 def main(args):
-    """
-
+    """ main file to train agents
     Args:
         param1: (args) parameter from command line
     """
@@ -25,22 +21,22 @@ def main(args):
     plot_score(scores)
 
 if __name__ == "__main__":
-    parser = argparse.ArgumentParser(description='Rainbow')
+    parser = argparse.ArgumentParser(description='MultiAgent')
     parser.add_argument('--seed', type=int, default=2, help='Random seed')
     parser.add_argument('--disable-cuda', action='store_true', help='Disable CUDA')
-    parser.add_argument('--actor_hidden_units', type=int, default=(512,256), metavar='SIZE', help='Network hidden size')
-    parser.add_argument('--critic_hidden_units', type=int, default=(512,256), metavar='SIZE', help='Network hidden size')
+    parser.add_argument('--actor_hidden_units', type=int, default=(512, 256), metavar='SIZE', help='Network hidden size')
+    parser.add_argument('--critic_hidden_units', type=int, default=(512, 256), metavar='SIZE', help='Network hidden size')
     parser.add_argument('--memory-capacity', type=int, default=int(1e6), metavar='CAPACITY', help='Experience replay memory capacity')
     parser.add_argument('--update_every', type=int, default=4, metavar='k', help='Frequency of sampling from memory')
-    parser.add_argument('--discount', type=float, default=0.99, metavar='γ', help='Discount factor')
-    parser.add_argument('--tau', type=float, default=1e-3, metavar='γ', help='Discount factor')
-    parser.add_argument('--actor-learning-rate', type=float, default=1e-4, metavar='η', help='Learning rate')
-    parser.add_argument('--critic-learning-rate', type=float, default=3e-4, metavar='η', help='Learning rate')
+    parser.add_argument('--discount', type=float, default=0.99, metavar='gamma', help='Discount factor')
+    parser.add_argument('--tau', type=float, default=1e-3, metavar='tau', help='Discount factor')
+    parser.add_argument('--actor-learning-rate', type=float, default=1e-4, metavar='n', help='Learning rate')
+    parser.add_argument('--critic-learning-rate', type=float, default=3e-4, metavar='n', help='Learning rate')
     parser.add_argument('--weight-decay', type=int, default=0)
     parser.add_argument('--batch-size', type=int, default=512, metavar='SIZE', help='Batch size')
     parser.add_argument('--model_num', default=0)
     parser.add_argument('--n_episodes', default=500)
     parser.add_argument('--env_num', default=1)
     parser.add_argument('--device', default="cpu")
-    
-    main(args = parser.parse_args())
+
+    main(parser.parse_args())
