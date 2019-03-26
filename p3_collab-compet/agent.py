e085baa4823c4ca8061c0aa0486715b770d18188
Christian Leininger <christianleininger42@gmail.com>
2019-03-26 11:52:20 -0700

    pylint checkstyle

--- p3_collab-compet/agent.py
+++ p3_collab-compet/agent.py
@@ -1,99 +1,101 @@
 """DDPG Agent """
-import random
 import numpy as np
 import torch
 import torch.nn.functional as F
-import copy
-
+from model import Actor, Critic
 from noise import OUNoise
 
 class DDPGAgent(object):
-    """ """
-    def __init__(self, config):
-        """
+    """ deep deterministic policy gradient """
+    def __init__(self, args, memory):
+        """ set actor and crtic network and hyperparameter
 
         Args:
-            param1: (config) 
+            param1: (args) commandline arguments
+            param2: (ReplayBuffer) experience stored
         """
-        self.config = config
-        self.device = config.device
-        self.seed = config.seed
-        self.gamma = config.gamma
+        self.args = args
+        self.device = args.device
+        self.seed = args.seed
+        self.gamma = args.discount
+        self.tau = args.tau
         # Actor online and target network
-        self.actor_online = Actor(config.action_size, config.state_size, config.actor_hidden_units, config.seed).to(config.device)
-        self.actor_target = Actor(config.action_size, config.state_size, config.actor_hidden_units, config.seed).to(config.device)
-        self.actor_optimizer = torch.optim.Adam(self.actor_online.parameters(), lr=config.actor_learning_rate)
+        self.actor_online = Actor(args.action_size, args.state_size, args.actor_hidden_units, args.seed).to(args.device)
+        self.actor_target = Actor(args.action_size, args.state_size, args.actor_hidden_units, args.seed).to(args.device)
+        self.actor_optimizer = torch.optim.Adam(self.actor_online.parameters(), lr=args.actor_learning_rate)
 
         # Critic
+        self.critic_online = Critic(args.action_size, args.state_size, args.critic_hidden_units, args.seed).to(args.device)
+        self.critic_target = Critic(args.action_size, args.state_size, args.critic_hidden_units, args.seed).to(args.device)
+        self.critic_optimizer = torch.optim.Adam(self.critic_online.parameters(), lr=args.critic_learning_rate)
 
-        self.critic_online = Critic(config.action_size, config.state_size, config.critic_hidden_units, config.seed).to(config.device)
-        self.critic_target = Critic(config.action_size, config.state_size, config.critic_hidden_units, config.seed).to(config.device)
-        self.critic_optimizer = torch.optim.Adam(self.critic_online.parameters(), lr=config.critic_learning_rate)
+        self.soft_update(self.actor_online, self.actor_target)
+        self.soft_update(self.critic_online, self.critic_target)
 
-        self.softe_update(self.actor_online, self.actor_target)
-        self.softe_update(self.critic_online, self.critic_target)
-
-        self.noise = OUNoise(config.action_size, config.seed)
-
-        self.memory = ReplayBuffer(config.buffer_size, config.batch_size, config.seed, config.device)
+        self.noise = OUNoise(args.action_size, args.seed)
+        self.memory = memory
 
     def noise_reset(self):
         """ set the  """
         self.noise.reset()
 
-    def act(self, states):
-        """ 
+    def act(self, states, explore=True):
+        """ actor maps the states with the policy to actions
+            and noise to explore
         Args:
            param1: (numpy) states
-        
+           param2: (Boole) true of add noise
+
         Return: actions between -1 and 1
         """
-
         states = torch.from_numpy(states).float().to(self.device)
-        self.actor_online.eval()  # deactivate backprob for inference 
+        self.actor_online.eval()  # deactivate backprob for inference
         with torch.no_grad():
             actions = self.actor_online(states).cpu().data.numpy()
         self.actor_online.train()
-        actions =  actions + self.noise.sample()
+        if explore:
+            actions = actions + self.noise.sample()
         return np.clip(actions, -1, 1)
 
     def learn(self, experience):
-        """
-
-
+        """ Update the actor and critic weights to improve policy
         Args:
             param1: (tuple) experience (s, a, r, s, done)
         """
+
         states, actions, rewards, next_states, dones = experience
 
         actor_actions = self.actor_target(next_states)   # use actor_tar network take action from next states
-        
-        Q_target_next = self.critic_target(next_states, actor_actions) # evalute the actions taken from actor
+        q_target_next = self.critic_target(actor_actions, next_states) # evalute the actions taken from actor
 
-        Q_target = reward +( gamma * Q_target_next * ( 1 - dones))  # bellman equation
+        q_target = rewards +(self.gamma * q_target_next * (1 - dones))  # bellman equation
 
-        Q_expected = self.critic_online(states, actions)  
+        q_expected = self.critic_online(actions, states)
 
         #---------------------------------- update critic ------------------------------------------------------
-        critic_loss = F.mse_loss(Q_expected, Q_target) # compute the loss critic online
+        critic_loss = F.mse_loss(q_expected, q_target) # compute the loss critic online
         self.critic_optimizer.zero_grad()
         critic_loss.backward()
         self.critic_optimizer.step()
-        
+
         #---------------------------------- update actor ------------------------------------------------------
-        pred_actions = self.actor_online(states) 
-        actor_loss = -self.critic_online(states, pred_actions).mean()  # compute loss actor
+        pred_actions = self.actor_online(states)
+        actor_loss = -self.critic_online(pred_actions, states).mean()  # compute loss actor
         self.actor_optimizer.zero_grad()
         actor_loss.backward()                                          # minimize loss
         self.actor_optimizer.step()
 
         #---------------------------------- update target networks ------------------------------------------------------
-        self.soft_update(self, online_model, target_model, tau=1):
-            """
-
-            Args:
-                param1:
-                par
-            """
+        self.soft_update(self.critic_online, self.critic_target, self.tau)
+        self.soft_update(self.actor_online, self.actor_target, self.tau)
 
 
+    def soft_update(self, online_model, target_model, tau=1):
+        """ copy weights from online model to the target model with tau
+        Args:
+            param1:(model) online_model
+            param2:(model) target model
+            param3:(float) update param between 0 and 1
+        """
+        for target_param, online_param in zip(target_model.parameters(), online_model.parameters()):
+            target_param.data.copy_(tau*online_param.data + (1.0-tau)*target_param.data)
