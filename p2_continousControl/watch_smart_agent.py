b114f08d77b4f85859487bd0b2f981d16ca9016c
Christian Leininger <christianleininger42@gmail.com>
2019-04-13 10:13:13 -0700

    pylint checkstyle

--- p2_continousControl/watch_smart_agent.py
+++ p2_continousControl/watch_smart_agent.py
@@ -3,7 +3,7 @@ import torch
 import numpy as np
 from unityagents import UnityEnvironment
 from config import Config
-from ddpg_agent import Agent
+from ddpg_agent import DDPGAgent
 
 
 def main(path=''):
@@ -12,7 +12,7 @@ def main(path=''):
        param1: (string) pathname for saved network weights
 
     """
-    env = UnityEnvironment(file_name='Reacher_Linux/Reacher.x86_64', no_graphics=True)
+    env = UnityEnvironment(file_name='Reacher_Linux/Reacher.x86_64', no_graphics=False)
     # get the default brain
     brain_name = env.brain_names[0]
     brain = env.brains[brain_name]
@@ -23,14 +23,23 @@ def main(path=''):
     num_agents = len(env_info.agents)
     print('Number of agents:', num_agents)
     config = Config()
-    config.discount = 0.99
+    config.n_agents = num_agents
+    config.gamma = 0.99
     config.state_dim = states.shape[1]
     config.action_dim = brain.vector_action_space_size
-    action_size = config.action_dim
-    state_size = config.state_dim
-    seed = 42
-
-    agent = Agent(state_size=state_size, action_size=action_size, n_agents=num_agents, seed=seed)
+    config.device = torch.device("cuda:0" if torch.cuda.is_available() else "cpu")
+    config.seed = 42
+    config.leak = 0.001
+    config.tau = 1e-3
+    config.hdl1 = 256
+    config.hdl2 = 128
+    config.hdl3 = 128
+    config.lr_actor = 0.001
+    config.lr_critic = 0.001
+    config.batch_size = 1024
+    config.weight_decay = 0.99
+    config.memory_capacity = int(1e6)
+    agent = DDPGAgent(config)
 
     agent.actor_local.load_state_dict(torch.load(path + 'checkpoint_actor.pth'))
     agent.critic_local.load_state_dict(torch.load(path + 'checkpoint_critic.pth'))
@@ -39,11 +48,11 @@ def main(path=''):
         scores = np.zeros(num_agents)
         env_info = env.reset(train_mode=False)[brain_name]
         states = env_info.vector_observations
-        agent.reset()
+        agent.reset_noise()
         total_steps = 0
         while True:
             total_steps += 1
-            actions = agent.act(states, False)
+            actions = agent.act(states, 0, False)
             env_info = env.step(actions)[brain_name]
             next_states = env_info.vector_observations
             reward = env_info.rewards
