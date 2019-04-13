8c9ade26858fb09651b4b57299a0f7cb79428b5c
Christian Leininger <christianleininger42@gmail.com>
2019-04-13 09:53:12 -0700

    pylint checkstyle

--- p2_continousControl/ddpg_agent.py
+++ p2_continousControl/ddpg_agent.py
@@ -1,24 +1,24 @@
+""" implementation of the DDPG algorithm  """
 import numpy as np
-import copy 
-from collections import namedtuple, deque
-import random
-from model import Actor, Critic
-from noise import OUNoise
-from memory import ReplayBuffer
 import torch
 import torch.nn.functional as F
 import torch.optim as optim
+from model import Actor, Critic
+from noise import OUNoise
+from memory import ReplayBuffer
 
 
-device = torch.device("cuda:0" if torch.cuda.is_available() else "cpu")
 
 
 class DDPGAgent(object):
+    """ class of the DDPG Agent """
     def __init__(self, config):
         """Initialize an Agent object.
-    
+
+        Args:
+            param1: (config)
         """
-    
+
         self.state_size = config.state_dim
         self.action_size = config.action_dim
         self.seed = np.random.seed(config.seed)
@@ -26,16 +26,17 @@ class DDPGAgent(object):
         self.batch_size = config.batch_size
         self.tau = config.tau
         self.gamma = config.gamma
+        self.device = config.device
         # Actor Network (w/ Target Network)
         self.actor_local = Actor(config).to(config.device)
         self.actor_target = Actor(config).to(config.device)
         self.actor_optimizer = optim.Adam(self.actor_local.parameters(), lr=config.lr_actor)
-    
+
         # Critic Network (w/ Target Network)
         self.critic_local = Critic(config).to(config.device)
         self.critic_target = Critic(config).to(config.device)
         self.critic_optimizer = optim.Adam(self.critic_local.parameters(), lr=config.lr_critic)
-    
+
         # Noise process
         self.noise = OUNoise(config)
 
@@ -49,10 +50,11 @@ class DDPGAgent(object):
         Returns a numpy array of shape [n_agents, n_actions]
         NOTE: clips actions to be between -1, 1
         Args:
-            states:    () one row of state for each agent [n_agents, n_actions]
-            add_noise: (bool) add noise to the actions?
+            states:    (torch) states
+            epsilon: (float)
+            add_noise: (bool) add noise to the actions
         """
-        states = torch.from_numpy(states).float().to(device)
+        states = torch.from_numpy(states).float().to(self.device)
         self.actor_local.eval()
         with torch.no_grad():
             actions = self.actor_local(states).cpu().data.numpy()
@@ -60,40 +62,39 @@ class DDPGAgent(object):
         if add_noise and epsilon > np.random.random():
             actions += [self.noise.sample() for _ in range(self.n_agents)]
         return np.clip(actions, -1, 1)
-    
+
     def reset_noise(self):
-        """ """
+        """ reset noise"""
         self.noise.reset()
 
     def learn(self):
         """Update policy and value parameters using given batch of experience tuples.
-        Q_targets = r + γ * critic_target(next_state, actor_target(next_state))     where:
         actor_target(state) -> action
         critic_target(state, action) -> Q-value
         """
-        if self.batch_size > self.memory.size(): 
-            return 
+        if self.batch_size > self.memory.size():
+            return
         states, actions, rewards, next_states, dones = self.memory.sample()
-        
-        # ---------------------------- update critic ---------------------------- 
-        
+
+        # ---------------------------- update critic ----------------------------
+
         # Get predicted next-state actions and Q values from target model
-        
+
         actions_next = self.actor_target(next_states)
         Q_targets_next = self.critic_target(next_states, actions_next)
-        
+
         # Compute Q targets for current states (y_i)
         Q_targets = rewards + (self.gamma * Q_targets_next * (1 - dones))
-        
+
         # Compute critic loss
         Q_expected = self.critic_local(states, actions)
         critic_loss = F.mse_loss(Q_expected, Q_targets)
-        
+
         # Minimize the loss
         self.critic_optimizer.zero_grad()
         critic_loss.backward()
         self.critic_optimizer.step()
-        
+
         # ---------------------------- update actor ---------------------------- #
         # Compute actor loss
         actions_pred = self.actor_local(states)
@@ -102,18 +103,17 @@ class DDPGAgent(object):
         self.actor_optimizer.zero_grad()
         actor_loss.backward()
         self.actor_optimizer.step()
-        
+
         # ----------------------- update target networks ----------------------- #
         self.soft_update(self.critic_local, self.critic_target)
         self.soft_update(self.actor_local, self.actor_target)
-        
+
     def soft_update(self, local_model, target_model):
         """Soft update model parameters.
-        θ_target = τ*θ_local + (1 - τ)*θ_target
-    
-        local_model: PyTorch model (weights will be copied from)
-        target_model: PyTorch model (weights will be copied to)
-        tau (float): interpolation parameter
+
+        Args:
+         param1: (torch network) local_model
+         param2: (torch network) target_model
         """
         for target_param, local_param in zip(target_model.parameters(), local_model.parameters()):
             target_param.data.copy_(self.tau*local_param.data + (1.0-self.tau)*target_param.data)
